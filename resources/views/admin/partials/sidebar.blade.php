2e446d0fa1cd430256f9146533db93dcca999446
Sanik0 <sanico.ronan.macugay@gmail.com>
2026-02-01 19:57:26 +0800

    sidebar active indicator

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -6,14 +6,34 @@
         </a>
         <ul class="space-y-2 font-medium">
             <li>
-                <a href="{{ route('admin.home')}}" class="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">
-                    <svg class="w-5 h-5 transition duration-75 group-hover:text-fg-brand" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6.025A7.5 7.5 0 1 0 17.975 14H10V6.025Z" />
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.5 3c-.169 0-.334.014-.5.025V11h7.975c.011-.166.025-.331.025-.5A7.5 7.5 0 0 0 13.5 3Z" />
+                <a href="{{ route('admin.home') }}"
+                    class="flex items-center px-2 py-1.5 rounded-base group
+                        hover:bg-neutral-tertiary
+                            {{ request()->routeIs('admin.home')
+                            ? 'text-fg-brand'
+                            : 'text-body'}}">
+                    <svg
+                        class="w-5 h-5 transition duration-75
+                        {{ request()->routeIs('admin.home')
+                        ? 'text-fg-brand'
+                        : '' }}"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24">
+
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M10 6.025A7.5 7.5 0 1 0 17.975 14H10V6.025Z" />
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M13.5 3c-.169 0-.334.014-.5.025V11h7.975c.011-.166.025-.331.025-.5A7.5 7.5 0 0 0 13.5 3Z" />
                     </svg>
-                    <span class="ms-3">Dashboard</span>
+
+                    <span class="ms-3
+                    {{ request()->routeIs('admin.home') 
+                    ? 'text-brand' 
+                    : 'text-black'}}">Dashboard</span>
                 </a>
             </li>
+
             <li>
                 <button type="button" class="flex items-center w-full justify-between px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
                     <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -37,10 +57,14 @@
             </li>
             <li>
                 <a href="{{ route('admin.users') }}" class="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">
-                    <svg class="shrink-0 w-5 h-5 transition duration-75 group-hover:text-fg-brand" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <svg class="shrink-0 w-5 h-5 transition duration-75
+                        {{request()->routeIs('admin.users') 
+                        ? 'text-fg-brand' : '' }}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
+                    <span class="flex-1 ms-3 whitespace-nowrap
+                        {{request()->routeIs('admin.users') 
+                        ? 'text-fg-brand' : '' }}">Users</span>
                 </a>
             </li>
             <li>
