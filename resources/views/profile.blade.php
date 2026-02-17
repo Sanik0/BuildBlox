f98d4d51accccff8affa5651804b6946ab3fe3bc
Sanik0 <sanico.ronan.macugay@gmail.com>
2026-02-17 10:55:36 +0800

    alert modal fixed

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,8 +1,26 @@
 @include('partials.header')
 
-<body class="bg-[#ffffff] text-[#1b1b18]">
+<body class="bg-[#ffffff] relative text-[#1b1b18]">
     <!-- HEADER -->
     @include('partials.navigation')
+    <!-- ALERT -->
+    @if(session('success'))
+    <div id="alert-5" class="flex mt-10 bg-black fixed bottom-5 inset-x-5 md:inset-x-10 md:left-auto z-999 sm:items-center w-fit max-w-md p-4 text-sm border-2" role="alert">
+        <svg class="w-4 h-4 fill-white shrink-0 mt-0.5 md:mt-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+        <span class="sr-only">Info</span>
+        <div class="ms-2 text-sm text-white overflow-hidden">
+            {{ session('success')}}
+        </div>
+        <button type="button" class="ms-auto -mx-1.5 -my-1.5 rounded focus:ring-2 focus:ring-neutral-tertiary p-1.5 hover:bg-gray-800 inline-flex items-center justify-center h-8 w-8 shrink-0 shrink-0" data-dismiss-target="#alert-5" aria-label="Close">
+            <span class="sr-only">Dismiss</span>
+            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" />
+            </svg>
+        </button>
+    </div>
+    @endif
     <!-- BUILD INFORMATION -->
     <section class="w-full mt-20 flex p-4 py-6 lg:py-8 items-center justify-center py-13">
         <div class="w-full max-w-screen-xl flex flex-col gap-13">
