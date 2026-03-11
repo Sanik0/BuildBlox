cd4bb7b90aa1cd53169138e2f3da7ac0c78741db
Sanik0 <sanico.ronan.macugay@gmail.com>
2026-03-11 21:35:30 +0800

    added preview modals for user

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -115,9 +115,12 @@
                                         <div id="user-{{ $user->user_id }}-dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
                                             <ul class="py-1 text-sm">
                                                 <li>
-                                                    <a href="" class="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200">
+                                                    <button type="button"
+                                                        data-modal-target="preview-{{ $user->user_id }}"
+                                                        data-modal-toggle="preview-{{ $user->user_id }}"
+                                                        class="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200">
                                                         Preview
-                                                    </a>
+                                                    </button>
                                                 </li>
                                                 <li>
                                                     <button type="button" class="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 text-red-500 dark:hover:text-red-400">
@@ -128,6 +131,38 @@
                                         </div>
                                     </td>
                                 </tr>
+                                <div id="preview-{{ $user->user_id }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                    <div class="relative p-4 w-full max-w-xl max-h-full">
+                                        <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                                            <div class="flex justify-between mb-4 rounded-t sm:mb-5">
+                                                <div class="text-lg text-gray-900 md:text-xl dark:text-white">
+                                                    <p class="font-bold">{{ $user->role == 1 ? 'Admin' : ($user->role == 2 ? 'Builder' : 'User') }}</p>
+                                                    <h3 class="font-semibold">{{ $user->first_name }} {{ $user->last_name }}</h3>
+                                                </div>
+                                                <button type="button" data-modal-toggle="preview-{{ $user->user_id }}" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex dark:hover:bg-gray-600 dark:hover:text-white">
+                                                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                            <dl>
+                                                <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Display Photo</dt>
+                                                <dd class="h-20 w-20 mb-2 border-2 border-black">
+                                                    <img class="object-cover h-full w-full object-center" src="{{ asset('images/' . $user->image) }}" alt="">
+                                                </dd>
+                                                <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Email</dt>
+                                                <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">{{ $user->email }}</dd>
+                                                <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Username</dt>
+                                                <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">{{ $user->username }}</dd>
+                                                <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Joined At</dt>
+                                                <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">{{ $user->created_at->format('F j, Y') }}</dd>
+                                                <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Biography</dt>
+                                                <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">{{ $user->biography ?? 'No biography yet.' }}</dd>
+                                            </dl>
+                                        </div>
+                                    </div>
+                                </div>
+
                                 @endforeach
                             </tbody>
                         </table>
@@ -254,61 +289,6 @@
                             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update User</button>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>
-        <!-- Read modal -->
-        <div id="readProductModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative p-4 w-full max-w-xl max-h-full">
-                <!-- Modal content -->
-                <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-                    <!-- Modal header -->
-                    <div class="flex justify-between mb-4 rounded-t sm:mb-5">
-                        <div class="text-lg text-gray-900 md:text-xl dark:text-white">
-                            <h3 class="font-semibold ">Mark John Macapagal</h3>
-                            <p class="font-bold">User</p>
-                        </div>
-                        <div>
-                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="readProductModal">
-                                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                                <span class="sr-only">Close modal</span>
-                            </button>
-                        </div>
-                    </div>
-                    <dl>
-                        <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Display Photo</dt>
-                        <dd class="h-20 w-20 mb-2 border-2 border-black">
-                            <img class="object-cover h-full w-full object-center" src="{{asset('/images/4k Minecraft Nature.jpg')}}" alt="">
-                        </dd>
-                        <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Email</dt>
-                        <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">markjohnmacapagal@gmail.com</dd>
-                        <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Username</dt>
-                        <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">markjohn_123</dd>
-                        <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Joined At</dt>
-                        <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">January 1, 2023</dd>
-                        <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Biography</dt>
-                        <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, officiis reprehenderit! Molestiae tempore culpa commodi incidunt repellendus eveniet voluptatum totam magni, provident iusto aut maxime enim. Eum explicabo asperiores, consequatur nesciunt eveniet corrupti quaerat blanditiis, officia totam accusantium nostrum nemo.</dd>
-                    </dl>
-                    <div class="flex justify-between items-center">
-                        <div class="flex items-center space-x-3 sm:space-x-4">
-                            <button type="button" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                <svg aria-hidden="true" class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                    <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                                </svg>
-                                Edit
-                            </button>
-                            <button type="button" class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Preview</button>
-                        </div>
-                        <button type="button" class="inline-flex items-center text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
-                            <svg aria-hidden="true" class="w-5 h-5 mr-1.5 -ml-1" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                            </svg>
-                            Delete
-                        </button>
-                    </div>
                 </div>
             </div>
         </div>
